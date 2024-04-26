0609038748aed619dd463894437f7734b4f532d0
SravanVullanki <85739869+SravanVullanki@users.noreply.github.com>
2024-04-26 21:49:40 +0530

    Update RetailerHome.js

--- src/retailer/RetailerHome.js
+++ src/retailer/RetailerHome.js
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react';
-import config from '../config'
 
 export default function RetailerHome() {
   const [retailerData, setRetailerData] = useState("");
